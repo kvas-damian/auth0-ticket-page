35dd32eefa794bdc9eb029875be39424233d703e
Damian Jozwiak <kvas-damian@users.noreply.github.com>
2018-03-11 13:37:17 +0100

    API fetching Zendesk ticket with comments

---
+++ api/zendesk.js
@@ -0,0 +1,28 @@
+const config = require('../config');
+const fetch = require("node-fetch");
+
+const API_URL = 'https://will-code-for-pizza.zendesk.com/api/v2/';
+
+function makeRequrest(path, method='GET') {
+	const url = `${API_URL}${path}`;
+	const basicAuthToken = new Buffer(`${config.ZENDESK_API_USER}/token:${config.ZENDESK_API_TOKEN}`, "utf8")
+		.toString("base64");
+
+	// TODO add error handling
+	return fetch(url, { headers: { Authorization: `Basic ${basicAuthToken}` } }).then(res => res.json());
+}
+
+module.exports = {
+	getTicket(ticketId) {
+		const ticketPath = `tickets/${ticketId}.json`;
+		const commentsPath = `tickets/${ticketId}/comments.json`;
+
+
+		return makeRequrest(ticketPath).then(ticket =>
+			makeRequrest(commentsPath).then(comments => {
+				ticket.ticket.comments = comments.comments;
+				return ticket.ticket;
+			})
+		);
+	}
+};
---
+++ config_sample.js
@@ -0,0 +1,4 @@
+module.exports = {
+	ZENDESK_API_TOKEN: 'FILL ME',
+	ZENDESK_API_USER: 'FILL ME'
+};
