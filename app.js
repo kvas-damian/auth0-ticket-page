00fde971b35abd99678aa5b90dbcae005f9ed1e9
Damian Jozwiak <kvas-damian@users.noreply.github.com>
2018-03-12 22:53:07 +0100

    Serve react App from express

--- app.js
+++ app.js
@@ -20,8 +20,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+
 app.use('/api', api);
+
+app.get('/*', function (req, res) {
+	res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
